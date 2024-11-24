99c1a556f46f1e82afd986c27375828a3d54abd2
Youssef Benihoud <benihoud@th-brandenburg.de>
2024-11-24 20:33:36 +0100

    [TWAPP-19] [TWAPP-20] Add User Feature | Updating Profile

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 
+
+// Import Cloudinary
+import { v2 as cloudinary } from 'cloudinary';
+
 dotenv.config();
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +26,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
