a2e14417ab60df08839ed6cd7385b8c808038799
Youssef Benihoud <benihoud@th-brandenburg.de>
2024-12-04 00:15:56 +0100

    [Bug] Fixing Blank Page

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,61 +1,32 @@
 import Notification from "../models/notification.model.js";
 
 export const getNotifications = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const notifications = await Notification.find({to: userId}).populate({
-            path: "from",
-            select: "username profileImg"
-        });
+	try {
+		const userId = req.user._id;
 
-        await Notification.updateMany({to:userId}, {read:true});
-        res.status(200).json(notifications);
+		const notifications = await Notification.find({ to: userId }).populate({
+			path: "from",
+			select: "username profileImg",
+		});
 
-    } catch (error) {
-        console.log("Error in getNotification Controller: ", error);
-        res.status(500).json({
-            error: "Internal Server Error!"
-        });
-    }
-}
+		await Notification.updateMany({ to: userId }, { read: true });
+
+		res.status(200).json(notifications);
+	} catch (error) {
+		console.log("Error in getNotifications function", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
 
 export const deleteNotifications = async (req, res) => {
-    try {
-        const userId = req.user._id;
+	try {
+		const userId = req.user._id;
 
-        await Notification.deleteMany({to:userId});
+		await Notification.deleteMany({ to: userId });
 
-        res.status(200).json({message: "Notifications deleted successfully!"});
-    } catch (error) {
-        console.log("Error in deleteNotifications controller: ", error);
-        res.status(500).json({error: "Internal Server Error"});
-    }
-}
-
-export const deleteNotification = async (req, res) => {
-    try {
-        const notificationId = req.params.id;
-        const userId = req.user._id;
-        const notification = await Notification.findById(notificationId);
-
-        if(!notification){
-            return res.status(404).json({
-                error: "Notification not found!"
-            })
-        }
-
-        if(notification.to.toString() !== userId.toString()){
-            return res.status(403).json({
-                error: "You are not allowed to delete this notification"
-            })
-        }
-
-        await Notification.findByIdAndDelete(notificationId);
-        res.status(200).json({
-            message: "Notification deleted successfully!"
-        });
-
-    } catch (error) {
-        
-    }
-}
+		res.status(200).json({ message: "Notifications deleted successfully" });
+	} catch (error) {
+		console.log("Error in deleteNotifications function", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,38 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
-import notificationRoutes from "./routes/notification.routes.js";
-
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
-import cookieParser from "cookie-parser";
-
-
-// Import Cloudinary
-import { v2 as cloudinary } from 'cloudinary';
 
 dotenv.config();
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/post", postRoutes);
-app.use("/api/notification", notificationRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
@@ -45,6 +43,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectMongoDB();
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
 });
